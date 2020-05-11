f4be01483a8ae0ff7a0814649f4bfce67bc84e73
lipeining <827154031@qq.com>
2020-05-11 17:21:55 +0800

    修改 test post router handler

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -30,12 +31,20 @@ func TestPostTable(t *testing.T) {
 	param := url.Values{}
 	param.Set("tblName", "card")
 	param.Add("num", "10000")
-	cols := "[{\"name\":\"id\",\"t\":\"id\",\"def\":\"\"},{\"name\":\"name\",\"t\":\"string\",\"def\":\"\"},{\"name\":\"card\",\"t\":\"string\",\"def\":\"\"},{\"name\":\"create_time\",\"t\":\"datetime\",\"def\":\"\"}]"
-	param.Add("cols", cols)
+	// cols := "[{\"name\":\"id\",\"t\":\"id\",\"def\":\"\"},{\"name\":\"name\",\"t\":\"string\",\"def\":\"\"},{\"name\":\"card\",\"t\":\"string\",\"def\":\"\"},{\"name\":\"create_time\",\"t\":\"datetime\",\"def\":\"\"}]"
+	// param.Add("cols", cols)
+	cols, _ := json.Marshal([]map[string]interface{}{
+		{ "name": "id", "t": "id", "def": ""},
+		{ "name": "name", "t": "string", "def": ""},
+		{ "name": "card", "t": "string", "def": ""},
+		{ "name": "create_time", "t": "datetime", "def": ""},
+	})
+	param.Add("cols", string(cols))
 	// param.Add("jsonPath", "\\.\\/tables\\/card\\.json")
 	requestData := strings.NewReader(param.Encode())
 	req, _ := http.NewRequest("POST", "/tables", requestData)
-	req.Header.Set("Content-type", "application/json")
+	// req.Header.Set("Content-type", "application/json")
+	req.Header.Set("Content-type", "application/x-www-form-urlencoded")
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
--- router.go
+++ router.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lipeining/fakesql/model"
 )
 // TableForm use to bind post /table form
 type TableForm struct {
-	TblName    string `json:"tblName" form:"tblName"`
+	TblName    string `form:"tblName" binding:"required"`
 	// JSONPath   string `json:"jsonPath" form:"jsonPath"`
-	Cols   string `json:"cols" form:"cols"`
-	Num        int    `json:"num" form:"num"`
+	Cols   string `form:"cols" binding:"required"`
+	Num        int    `form:"num" binding:"required"`
 }
 
 func setupRouter() *gin.Engine {
@@ -25,19 +29,30 @@ func setupRouter() *gin.Engine {
 		c.String(http.StatusOK, "home")
 	})
 	r.POST("/tables", func(c *gin.Context){
-		var tableForm TableForm
-		if err := c.ShouldBindJSON(&tableForm); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		tblName, num, defCols := tableForm.TblName, tableForm.Num, tableForm.Cols
-		fmt.Println(tblName, num, defCols)
-		cols, err := ParseJSONColumn(tblName)
+		// var tableForm TableForm
+		// if err := c.ShouldBind(&tableForm); err != nil {
+		// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		// 	return
+		// }
+		// tblName, num, defCols := tableForm.TblName, tableForm.Num, tableForm.Cols
+		// cols, err := ParseJSONColumn(tblName)
+		// if err != nil {
+		// 	fmt.Println("parse json file error ", err)
+		// 	c.JSON(http.StatusBadRequest, err)
+		// 	return
+		// }
+		// makeColumn := MakeColumnFuncFactory(cols)
+		tblName := c.PostForm("tblName")
+		num, _ := strconv.Atoi(c.DefaultPostForm("Num", "10000"))
+		colsStr := c.PostForm("cols")
+		var cols []model.Column
+		err := json.Unmarshal(bytes.NewBufferString(colsStr).Bytes(), &cols)
 		if err != nil {
-			fmt.Println("parse json file error ", err)
+			fmt.Println("un marshal cols error ", err)
 			c.JSON(http.StatusBadRequest, err)
 			return
 		}
+		fmt.Println(tblName, num, cols)
 		makeColumn := MakeColumnFuncFactory(cols)
 		err = Writetxt(tblName, num, makeColumn)
 		if err != nil {
